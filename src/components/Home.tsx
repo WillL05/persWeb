da97b7d61007979f1d69e2cd4748747694d922a0
WillL05 <vsxq4148@leeds.ac.uk>
2025-05-30 17:08:23 +0100

    lol

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import '../App.css'
 export default function Home() {
 
     return(<>
-            <div className = "PageTop">
+            <header className = "PageTop">
                 <div className="Title">
                     <p>Will</p>
                 </div>
@@ -12,15 +12,16 @@ export default function Home() {
                     <button className = "btn1" type={"button"}>About</button>
                     <button className = "btn2" type={"button"}>Contact</button>
                 </div>
+            </header>
+            <div className = "body">
+                <img className = "image" src='src/assets/Will.webp' alt = "Will"/>
+                <h1 className="Header_Txt">Hi I'm Will</h1>
+                <div className="text">
+                    <p>I'm a computer science student at the University of Leeds with an interest in software engineering</p>
+                </div>
             </div>
 
 
-            <img className = "image" src='src/assets/Will.webp' alt = "Will"/>
-
-            <div className="text">
-
-                <p>Hi! I'm Will, a first, going second year computer science student at the University of Leeds.  </p>
-            </div>
         </>
     )
 }
